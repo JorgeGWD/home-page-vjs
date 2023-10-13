4c77856dcf4d506762e95dafd2f8a6d8f9167137
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-13 12:58:01 -0500

    Adding type in props

--- src/components/commons/button/button.js
+++ src/components/commons/button/button.js
@@ -1,4 +1,4 @@
-export default class MyButton extends HTMLElement {
+class Button extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
@@ -8,31 +8,43 @@ export default class MyButton extends HTMLElement {
         linkElement.setAttribute('href', '../../../../src/components/commons/button/button.css');
 
         this.shadowRoot.appendChild(linkElement);
+
         this.button = document.createElement('button');
         this.shadowRoot.appendChild(this.button);
     }
 
     static get observedAttributes() {
-        return ['text', 'class'];
+        return ['text', 'class', 'type'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'text' || name === 'class') {
+        if (name === 'text' || name === 'class' || name === 'type') {
             this.updateButton();
         }
     }
 
     connectedCallback() {
         this.updateButton();
+        this.setupEvents();
     }
 
     updateButton() {
+        // Property types
         const text = this.getAttribute('text') || 'Send';
-        const buttonClass = this.getAttribute('class') || '';
+        const buttonClass = this.getAttribute('class') || ''; 
+        const buttonType = this.getAttribute('type') || 'button';
 
         this.button.textContent = text;
         this.button.className = buttonClass;
+        this.button.type = buttonType;
+    }
+
+    setupEvents() {
+        this.button.addEventListener('click', (event) => {
+            // Propagate custom event 'my-button-click' instead of 'click'
+            this.dispatchEvent(new CustomEvent('my-button-click', { bubbles: true }));
+        });
     }
 }
 
-customElements.define('my-button', MyButton);
+customElements.define('my-button', Button);
