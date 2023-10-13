d853ff96c1c08a71762e68752a15c61cad6f067d
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-13 16:02:23 -0500

    Adding stars and man-min pices

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -41,20 +41,46 @@ class Carousel extends HTMLElement {
                 </div>
                 <div class="carousel__slider">
                     ${this.products.map(product => `
-                            <div class="carousel__slider-card" key=${product.id}>
-                                <div class="carousel__slider-card-body" style="background-image:url(${product.featuredImage.url})">
+                        <div class="carousel__slider-card" key=${product.id}>
+                            <div class="carousel__slider-card-body" style="background-image:url(${product.featuredImage.url})">
                                 <my-button text="See more" class="__secondary"></my-button>
-                                </div>
-                                <p>${product.title}</p>
                             </div>
-                        `
-                    ).join('')}
+                            <div class="carousel__slider-card-footer">
+                                <p>${product.title}</p>
+                                <div class="carousel__slider-card-footer-info">
+                                    <span class="carousel__slider-card-footer-ranking">
+                                        ${this.getStarRating(product.tag)} (${product.tags[0]})
+                                    </span>
+                                    <div class="carousel__slider-card-footer-info-offer">
+                                        <span class="carousel__slider-card-footer-info-offer-max">${product.prices.max.amount}</span>
+                                        <span class="carousel__slider-card-footer-info-offer-min">${product.prices.min.amount}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `).join('')}
                 </div>
                 <div class="carousel__bottom">
                     <my-button text="Browse all products" class="__principal"></my-button>
                 </div>
             </section>
-        `
+        `;
+    }
+
+    getStarRating(puntuacion) {
+        if (0 <= puntuacion <= 100) {
+            return "⭐";
+        } else if (100 < puntuacion <= 200) {
+            return "⭐⭐";
+        } else if (200 < puntuacion <= 300) {
+            return "⭐⭐⭐";
+        } else if (300 < puntuacion <= 400) {
+            return "⭐⭐⭐⭐";
+        } else if (400 < puntuacion <= 500) {
+            return "⭐⭐⭐⭐⭐";
+        } else {
+            return "Puntuación fuera de rango";
+        }
     }
 }
 
